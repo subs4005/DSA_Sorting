1c8c8ac0713ff133f74abe3808aa6dc1308dcf24
subs4005 <4005subs@gmail.com>
2025-02-08 18:07:30 +0530

    bubleSortCode

---
+++ bubbleSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<vector>
+// #include<algorithm>
+using namespace std;
+
+void bubbleSort(vector<int> &v){
+    int n = v.size();
+
+    for(int i = 0; i< n-1; i++ ){
+        for(int j = 0; j < n-i-1; j++){
+            if(v[j] > v[j+1]){
+                swap(v[j], v[j+1]);
+            }
+        }
+    }
+        return;
+
+}
+
+
+int main(){
+    int n;
+    cout<<"given no. to arr "; 
+    cin>>n;
+    vector<int> v(n);
+    for(int i = 0; i < v.size(); i++){
+        cin>>v[i];
+    }
+
+    bubbleSort(v);
+
+    for(int i = 0;i < n; i++ ){
+        cout<<v[i] <<" ";
+    }cout<<endl;
+
+}
